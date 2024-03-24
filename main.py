c8ac82c4dd9d6cec4379e409e36f802cdc4a993d
Luis Mendez <luis3mendez6@gmail.com>
2024-03-24 17:30:14 -0600

    Inicializacion de programa principal

---
+++ main.py
@@ -0,0 +1,20 @@
+"""Modulo principal para toma de snapwhoots de camaras de red"""
+
+from email.message import EmailMessage
+from tinydb import TinyDB, Query
+import datetime as dt
+import ssl
+import smtplib
+import cv2
+import nmap
+
+
+def run_prgram():
+    """Lazo de ejecucion principal"""
+
+    return None
+
+
+if __name__ == "__main__":
+
+    run_prgram()
