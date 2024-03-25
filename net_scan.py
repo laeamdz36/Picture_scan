18b17706f84e8d35e0fc9dc4cf0894461d16ddd6
HP Luis Mdz <luis3mendez6@gmail.com>
2024-03-24 21:33:09 -0600

    Modificaciones en net scan

--- net_scan.py
+++ net_scan.py
@@ -1,7 +1,7 @@
 """Modulo para el escaneo de red"""
 
 import nmap
-import config as cfg
+# import config as cfg
 
 
 def read_config() -> dict:
@@ -14,3 +14,24 @@ def read_config() -> dict:
 
 def scan_network():
     """Escaneo de red, obtencion de hosts"""
+
+    # Local configurations
+    ip_range = "192.168.0.1/24"
+    pars = "-sP"
+
+    nm = nmap.PortScanner()
+    nm.scan(ip_range, arguments=pars)
+
+    for host in nm.all_hosts():
+        if "mac" in nm[host]["addresses"]:
+            print(f"{host} - {nm[host]['addresses']['mac']}")
+
+
+def run_program():
+    """Testing program fnuction"""
+    scan_network()
+
+
+if __name__ == "__main__":
+
+    run_program()
